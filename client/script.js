8927779eaa85a3cbc66f1df1dd9256dbe1dfb49b
kd5200 <kareemdavis18@yahoo.com>
2023-09-07 12:18:53 -0400

    server URL change

--- client/script.js
+++ client/script.js
@@ -76,7 +76,7 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv);
 
-  const response = await fetch("https://chatgpt-clone3-0.onrender.com", {
+  const response = await fetch("https://chatgpt-clone3-0.onrender.com/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
